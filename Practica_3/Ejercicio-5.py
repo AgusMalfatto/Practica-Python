1706de56502c76957c4a592f87db60dc7e3c25fa
AgusMalfatto <agus.malfatto@hotmail.com>
2022-04-29 16:42:03 -0300

    29/04/2022

--- Practica_3/Ejercicio-5.py
+++ Practica_3/Ejercicio-5.py
@@ -12,25 +12,33 @@ import string
 password = input("Ingrese su contraseña: ")
 
 lista_password = list(password)
+
+# Genero una cadena con todas las letras del abecedario (mayúsculas y minúsculas)
 cadena = list(string.ascii_letters)
 contador_letra = 0
-contador_num= 0
+contador_num = 0
 control = 0
 
-if len(lista_password) < 8:
-    control = 1
-
-if control == 0:
+# Corroboro la longitud de la contraseña
+if len(lista_password) > 8:
+    # Recorro la contraseña ingresada caracter por caracter
     for i in lista_password:
+        # Evaluo si el caracter pertenece al abecedario
         if i in cadena:
             contador_letra += 1
+        # Evaluo que el caracter sea un número
         elif i.isdigit():
             contador_num += 1
+        # Si no pertenece al abecedario ni es un número, la contraseña es incorrecta
         else:
             control = 1
             break
+    
         
 '''
+
+                  PRIMERA FORMA QUE USÉ
+
 if control == 0:
     for i in lista_password:
         if i.isdigit():
@@ -49,9 +57,9 @@ if control == 0:
         control = 1
 '''
 
-if control == 1 or contador_num < 2 or contador_letra == 0:
-    mensaje = "La contraseña no es correcta"
-else:
+if control != 1 and contador_num >= 2 and contador_letra > 0:
     mensaje = "La contraseña es correcta"
+else:
+    mensaje = "La contraseña no es correcta"
 
 print(mensaje)
--- Practica_3/Ejercicio-6.py
+++ Practica_3/Ejercicio-6.py
@@ -1,5 +1,6 @@
 cadena = input("Ingrese la cadena: ")
 
+# Creo una lista, en la que cada elemento es una lista de las letras dispuestas en el teléfono
 lista = (("A", "B", "C"), ("D", "E", "F"), ("G", "H", "I"), ("J", "K", "L"), ("M", "N", "O"), 
 ("P", "Q", "R", "S"), ("T", "U", "V"), ("W", "X", "Y", "Z"))
 
@@ -7,10 +8,13 @@ lista_cadena = list(cadena)
 
 numero = []
 
+# Recorro la cadena ingresada por el usuario
 for i in lista_cadena:
+    # Recorro la lista creada anteriormente
     for k in lista:
         if i in k:
             indice = lista.index(k)
+            # Las letras comienzan en el número 2 en el teclado telefónico, por eso sumo 2 al índice
             numero.append(indice + 2)
             break
 

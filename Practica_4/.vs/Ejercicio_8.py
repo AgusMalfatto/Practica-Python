90bedf722dfe6629c29924b20a5b9991fba66600
AgusMalfatto <agus.malfatto@hotmail.com>
2022-05-12 19:18:14 -0300

    Ejercicio 8

--- Practica_4/.vs/Ejercicio_8.py
+++ Practica_4/.vs/Ejercicio_8.py
@@ -24,8 +24,35 @@ obtenerDominios(c) se obtiene:
 contarTld(c) se obtiene: {'cz': 2, 'de': 3, 'cn': 2, 'cl': 8}
 '''
 
+def Obtener_Dominios(correos):
+    lista_dominios = []
+    lista_correos = []
+    for correo in correos:
+        correo_dominio = correo.split("@")
+        lista_correos.append(correo_dominio)
+    for dominios in lista_correos:
+        if not(dominios[1] in lista_dominios):
+            lista_dominios.append(dominios[1])
+    lista_dominios = sorted(lista_dominios)
+    return lista_dominios
+
+def Contar_Id(correos):
+    lista_tld = []
+    for correo in correos:
+        for i in range(len(correo) - 1, -1, -1):
+            if correo[i] == '.' and not(correo[i+1: len(correo)] in genericos):
+                lista_tld.append(correo[i+1: len(correo)])
+                break
+    print(lista_tld)
+
+            
+
 correos = ['fulano@usm.cl', 'erika@lala.de', 'li@zi.cn', 'a@a.net', 'gudrun@lala.de',
 'otto.von.d@lorem.ipsum.de', 'org@cn.de.cl', 'yayita@abc.cl', 'jozin@baz.cz', 'jp@foo.cl',
 'dawei@hao.cn', 'pepe@gmail.com', 'ana@usm.cl', 'polo@hotmail.com', 'fer@x.com',
 'ada@alumnos.usm.cl', 'dj@foo.cl', 'jan@baz.cz', 'd@abc.cl']
 
+genericos = {'com', 'gov', 'edu', 'org', 'net', 'mil'}
+
+lista_dominios = Obtener_Dominios(correos)
+Contar_Id(correos)
